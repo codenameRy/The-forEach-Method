a3edf69bdf6695f4589fc80ef857fdf99109ed7c
Ryan Lee <ryanm.lee11@gmail.com>
2020-08-24 02:37:23 +0000

    Challenges - Refactor for Loop and forEach for multiplication

--- index.js
+++ index.js
@@ -35,3 +35,52 @@ function adder(number){
 
 numbers2.forEach(adder);
 console.log(sum2);
+
+//Challenge 1 - Moving away from for Loops
+
+//Refactor this code below
+function handlePosts() {
+    var posts = [
+      { id: 23, title: 'Daily JS News' },
+      { id: 52, title: 'Code Refactor City' },
+      { id: 105, title: 'The Brightest Ruby' }
+    ];
+    
+    for (var i = 0; i < posts.length; i++) {
+      console.log(posts[i]);
+    }
+}
+
+console.log(handlePosts())
+
+//Solution 1
+function handlePosts2() {
+    var posts = [
+      { id: 23, title: 'Daily JS News' },
+      { id: 52, title: 'Code Refactor City' },
+      { id: 105, title: 'The Brightest Ruby' }
+    ];
+    
+    posts.forEach(function(post){
+      console.log(post)
+    })
+}
+
+console.log(handlePosts2())
+
+//Challenge 2 - Processing values
+//Calculate area of images
+
+//Solution
+var images = [
+  { height: 10, width: 30 },
+  { height: 20, width: 90 },
+  { height: 54, width: 32 }
+];
+var areas = [];
+
+images.forEach(function(image) {
+  areas.push(image.height * image.width)
+})
+
+console.log(areas)
