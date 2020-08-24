b785661f2feddbe1dec3468b9c0b53379ae488a8
Ryan Lee <ryanm.lee11@gmail.com>
2020-08-24 01:43:07 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+//Array Helper Methods - The Easiest Way to Write Better Code
+
+//The forEach Helper
+var colors = ['red', 'blue', 'green'];
+
+for (var i = 0; i < colors.length; i++) {
+  console.log(colors[i])
+}
+
+colors.forEach((color) => {
+  console.log(color)
+})
