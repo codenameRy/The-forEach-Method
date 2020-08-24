f47dafede1f6190a8708b3120d253d1fa8fb0b22
Ryan Lee <ryanm.lee11@gmail.com>
2020-08-24 02:16:57 +0000

    Challenge - Traditional looping and sum of array

--- index.js
+++ index.js
@@ -10,3 +10,28 @@ for (var i = 0; i < colors.length; i++) {
 colors.forEach((color) => {
   console.log(color)
 })
+
+//forEach Continued
+//Sum of numbers in Array
+
+//Create array of numbers
+var numbers = [1,2,3,4,5]
+//Variable to hold sum
+var sum = 0;
+//Loop over array, incrementing the sum Variable
+numbers.forEach(function(number){
+  sum += number;
+})
+//print the sum variable
+console.log(sum);
+
+//Declare the function externally
+var numbers2 = [1,2,3,4,5,6]
+//Variable to hold sum
+var sum2 = 0;
+function adder(number){
+  sum2 += number;
+}
+
+numbers2.forEach(adder);
+console.log(sum2);
